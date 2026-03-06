0a25e0300e7f8f6069d8fc96932434cca2d67aca
nahidx07 <muhammadnahidhossen70@gmail.com>
2026-03-07 00:47:09 +0600

    Create Loader.js

---
+++ components/Loader.js
@@ -0,0 +1,3 @@
+export default function Loader() {
+  return <div className="flex justify-center items-center h-40"><div className="animate-spin rounded-full h-8 w-8 border-b-2 border-green-500"></div></div>;
+}
